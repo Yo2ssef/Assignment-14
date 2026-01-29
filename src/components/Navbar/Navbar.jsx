9d753bd838fc6d95a668f6618446c8c702ca67e6
Yo2ssef <youssefahmed2780@gmail.com>
2026-01-29 14:58:21 +0200

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import logo from "../../assets/logo.png";
 export default function Navbar() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg py-3 fixed-top">
+      <nav className="navbar navbar-expand-lg bg-black py-3 fixed-top">
         <div className="container d-flex justify-content-between align-items-center">
           <div className="d-flex align-items-center gap-2">
             <img src={logo} alt="logo" style={{ width: "45px" }} />
@@ -45,13 +45,16 @@ export default function Navbar() {
                 </NavLink>
               </div>
               <div className="nav-item">
-                <NavLink to="/WhoAreWe" className="nav-link text-white-50 px-3 py-2 rounded-5 border-0">
+                <NavLink
+                  to="/WhoAreWe"
+                  className="nav-link text-white-50 px-3 py-2 rounded-5 border-0"
+                >
                   من نحن
                 </NavLink>
               </div>
             </div>
 
-            <div className="d-lg-none d-flex flex-column gap-2 mt-3 w-100 text-center">
+            <div className="d-lg-none d-flex flex-column gap-2 mt-3 col-12 text-center">
               <button className="text-white-50 bg-transparent border-0 ">
                 <i className=" fa-solid fa-magnifying-glass"></i> بحث
               </button>
